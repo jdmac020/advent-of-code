853585255e9d398c486887463dc174dadcc149a1
jdmac020 <john.macgregor@gmail.com>
2021-12-04 15:36:46 -0500

    Day 2 Puzzle 2; prettified

--- 2021/DayOne/aoc_day_one/src/main.rs
+++ 2021/DayOne/aoc_day_one/src/main.rs
@@ -5,7 +5,7 @@ mod data;
 fn main() {
     board_ship();
     map_depth_chart();
-    println!("Final position is: {}", navigation::calculate_end_position(data::get_nav_route()))
+    plot_destination_position();
 }
 
 fn board_ship() {
@@ -24,9 +24,21 @@ fn board_ship() {
 
 fn map_depth_chart() {
     println!("Day One: Measure the Deep\n");
-    println!("\tChecking sonar pings... \n\t...\n\t...\n\t...\n");
+    println!("\tChecking sonar pings... \n\t\t...\n\t\t...\n\t\t...");
     let readings = data::get_sonar_readings();
     println!("\tNumber of readings: {}", readings.iter().count());
     println!("\tNumber of fuzzy increases: {}", sonar::get_depth_increases(readings.clone()));
     println!("\tNumber of precise increases: {}\n\n", sonar::get_depth_increase_with_precision(readings.clone()));
+}
+
+fn plot_destination_position() {
+    println!("Day Two: Plot Destination\n");
+    println!("\tEvaluating NavRoute... \n\t\t...\n\t\t...\n\t\t...");
+    println!("\tDestination Coordinates: {}", navigation::calculate_end_position(data::get_nav_route()));
+    println!("\t\t...\n\t\t...");
+    println!("\tError! Destination outside navigable waters\n");
+    println!("\tRecalculating...\n\t\t...\n\t\t...\n\t\t...");
+    println!("\tFactoring in Control Upgrades...\n\t\t...\n\t\t...\n\t\t...");
+    println!("\t\t...\n\t\t...");
+    println!("\t*Actual* destination coordinates: {}\n\n", navigation::calculate_actual_end_position(data::get_nav_route()));
 }
--- 2021/DayOne/aoc_day_one/src/navigation.rs
+++ 2021/DayOne/aoc_day_one/src/navigation.rs
@@ -1,43 +1,47 @@
-fn parse_command(command: &str) -> (i32,i32) {
-    let plane;
-    let adjust;
+const FORWARD_DIRECTION_STR: &str = "forward";
+const FORWARD_DIRECTION: i32 = 0;
+const DOWN_DIRECTION_STR: &str = "down";
+const DOWN_DIRECTION: i32 = 1;
+const UP_DIRECTION_STR: &str = "up";
+const UP_DIRECTION: i32 = 2;
+
+struct PositionChange {
+    pub horizontal: i32,
+    pub vertical: i32
+}
+
+struct Command {
+    pub direction: i32,
+    pub amount: i32
+}
+
+fn parse_command(command: &str) -> Command {
+    let mut parsed_command = Command { direction: 0, amount: 0 };
     let parts: Vec<&str> = command.split(' ').collect();
 
     let direction = parts[0];
     let amount = parts[1].parse::<i32>().unwrap();
-
-    calculate_position_adjust(direction, amount);
-
-    if direction == "forward" {
-        plane = 0;
-        adjust = amount
-    } else {
-        plane = 1;
-
-        if direction == "down" {
-            adjust = amount
-        } else {
-            adjust = amount * -1;
+    match direction {
+        FORWARD_DIRECTION_STR => {
+            parsed_command.direction = FORWARD_DIRECTION;
+            parsed_command.amount = amount;
+        },
+        DOWN_DIRECTION_STR => {
+            parsed_command.direction = DOWN_DIRECTION;
+            parsed_command.amount = amount;
+        }, 
+        UP_DIRECTION_STR => {
+            parsed_command.direction = UP_DIRECTION;
+            parsed_command.amount = amount * -1;
         }
+        _ => println!("Might as well surface and ask for directions now!")
     }
 
-    return (plane, adjust);
+    return parsed_command;
 }
 
-fn calculate_position_adjust(direction:&str, amount:i32) -> (i32, i32) {
-    return (0,0);
-}
-
-fn calculate_aim(direction:&str, amount:i32) -> i32 {
-    if direction == "down" {
-        return amount;
-    }
-
-    if direction == "up" {
-        return amount * -1;
-    }
-
-    return 0;
+fn calculate_position_adjust(amount:i32, aim:i32) -> PositionChange {
+    return PositionChange { horizontal: amount, vertical: amount * aim }
 }
 
 pub fn calculate_end_position(route: Vec<&str>) -> i32 {
@@ -46,10 +50,32 @@ pub fn calculate_end_position(route: Vec<&str>) -> i32 {
 
     for entry in route {
         let command = parse_command(entry);
-        if command.0 == 0 {
-            x_pos = x_pos + command.1;
+        if command.direction == 0 {
+            x_pos = x_pos + command.amount;
         } else {
-            y_pos = y_pos + command.1;
+            y_pos = y_pos + command.amount;
+        }
+    }
+
+    return x_pos * y_pos;
+}
+
+pub fn calculate_actual_end_position(route: Vec<&str>) -> i32 {
+    let mut aim : i32 = 0;
+    let mut x_pos : i32 = 0;
+    let mut y_pos : i32 = 0;
+
+    for entry in route {
+        let command = parse_command(entry);
+
+        if command.direction != FORWARD_DIRECTION {
+            aim = aim + command.amount;
+        }
+
+        if command.direction == FORWARD_DIRECTION {
+            let new_pos = calculate_position_adjust(command.amount, aim);
+            x_pos = x_pos + new_pos.horizontal;
+            y_pos = y_pos + new_pos.vertical;
         }
     }
 
@@ -62,17 +88,35 @@ mod tests {
 
     #[test]
     fn parse_command_returns_0_5_tuple() {
-        assert_eq!((0, 5), parse_command("forward 5"));
+        let expected = Command {
+            direction: FORWARD_DIRECTION,
+            amount: 5
+        };
+        let result = parse_command("forward 5");
+        assert_eq!(expected.direction, result.direction);
+        assert_eq!(expected.amount, result.amount);
     }
 
     #[test]
     fn parse_command_returns_1_10_tuple() {
-        assert_eq!((1,10), parse_command("down 10"));
+        let expected = Command {
+            direction: DOWN_DIRECTION,
+            amount: 10
+        };
+        let result = parse_command("down 10");
+        assert_eq!(expected.direction, result.direction);
+        assert_eq!(expected.amount, result.amount);
     }
 
     #[test]
     fn parse_command_returns_1_neg5_tuple() {
-        assert_eq!((1,-5), parse_command("up 5"));
+        let expected = Command {
+            direction: UP_DIRECTION,
+            amount: -5
+        };
+        let result = parse_command("up 5");
+        assert_eq!(expected.direction, result.direction);
+        assert_eq!(expected.amount, result.amount);
     }
 
     #[test]
@@ -89,17 +133,32 @@ mod tests {
     }
 
     #[test]
-    fn calculate_aim_returns_0_for_forward_25() {
-        assert_eq!(0, calculate_aim("forward", 25));
+    fn calculate_actual_end_position_returns_900() {
+        let route = vec![
+            "forward 5",
+            "down 5",
+            "forward 8",
+            "up 3",
+            "down 8",
+            "forward 2"
+        ];
+        assert_eq!(900, calculate_actual_end_position(route));
     }
 
     #[test]
-    fn calculate_aim_returns_5_for_down_5() {
-        assert_eq!(5, calculate_aim("down", 5));
+    fn calculate_position_adjust_returns_horizontal_5_for_forward_five_no_aim() {
+        let expected = PositionChange { horizontal: 5, vertical: 0 };
+        let result = calculate_position_adjust(5, 0);
+        assert_eq!(expected.horizontal, result.horizontal);
+        assert_eq!(expected.vertical, result.vertical);
     }
+    
 
     #[test]
-    fn calculate_aim_returns_neg7_for_up_7() {
-        assert_eq!(-7, calculate_aim("up", 7));
+    fn calculate_position_adjust_returns_horizontal_5_vertical_25_for_aim_5() {
+        let expected = PositionChange { horizontal: 5, vertical: 25 };
+        let result = calculate_position_adjust(5, 5);
+        assert_eq!(expected.horizontal, result.horizontal);
+        assert_eq!(expected.vertical, result.vertical);
     }
 }
