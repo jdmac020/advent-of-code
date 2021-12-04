5915187ea49143b57dc402b6a231d136202e35d3
jdmac020 <john.macgregor@gmail.com>
2021-12-04 10:22:29 -0500

    Day1 Puzzle1 Solved

--- 2021/DayOne/aoc_day_one/src/main.rs
+++ 2021/DayOne/aoc_day_one/src/main.rs
@@ -1,17 +1,24 @@
+mod data;
+
 fn main() {
-    
+    println!("Checking sonar pings... \n...\n...\n...\n");
+    let readings = data::get_sonar_readings();
+    let readings_count = readings.iter().count();
+    let increases = check_vector_via_zip(readings);
+    println!("\tNumber of readings: {}", readings_count);
+    println!("\tNumber of increases: {}", increases);
 }
 
 fn check_increase(first: &i32, second: &i32) -> bool {
     return first < second;
 }
 
-fn check_vector(numbers: Vec<i32>) -> usize {
+fn check_vector_via_zip(numbers: Vec<i32>) -> usize {
     let output = numbers.iter().zip(numbers.iter().skip(1)).filter(|cur| check_increase(cur.0,cur.1));
     return output.count();
 }
 
-fn check_vector2(numbers: Vec<i32>) -> usize {
+fn check_vector_via_window(numbers: Vec<i32>) -> usize {
     let items = numbers.windows(2).filter(|w| check_increase(&w[0], &w[1])).count();
     return items;
 }
@@ -39,13 +46,13 @@ mod tests {
     #[test]
     fn returns_three_zip() {
         let numbers = vec![1,2,1,2,3];
-        assert_eq!(3, check_vector(numbers));
+        assert_eq!(3, check_vector_via_zip(numbers));
     }
 
     #[test]
-    fn returns_three_windows() {
+    fn returns_three_window() {
         let numbers = vec![1,2,1,2,3];
-        assert_eq!(3, check_vector2(numbers));
+        assert_eq!(3, check_vector_via_window(numbers));
     }
 
     #[test]
@@ -53,21 +60,17 @@ mod tests {
         let numbers = vec![1,2,1,2,3];
         assert_eq!(vec![0,1,2,1,2,3], bump_vec(numbers));
     }
-
-    // these two fail because we're not capturing all the increases
-    // all the increases between pairs, like (index0, index1) and then (index2,index3), are captured
-    // but increases that span pairs like (index1, index2) and (index3, index4) are being missed
-    // recognizing that is about all I had at 1045pm on a Thursday
+    
     #[test]
     fn returns_seven_zip() {
-        let numbers = vec![200,208,210,200,207,240,269,260,263];
-        assert_eq!(7, check_vector(numbers));
+        let numbers = vec![199,200,208,210,200,207,240,269,260,263];
+        assert_eq!(7, check_vector_via_zip(numbers));
     }
 
     
     #[test]
-    fn returns_seven_windows() {
-        let numbers = vec![200,208,210,200,207,240,269,260,263];
-        assert_eq!(7, check_vector2(numbers));
+    fn returns_seven_window() {
+        let numbers = vec![199,200,208,210,200,207,240,269,260,263];
+        assert_eq!(7, check_vector_via_window(numbers));
     }
 }
